96be45f92a8cb4a9a53a5059f24626e6213acde4
Avery <AveryGaskins93@gmail.com>
2025-09-08 23:07:11 -0400

    addind resize

--- ImageProcessor/images.py
+++ ImageProcessor/images.py
@@ -4,7 +4,8 @@ img = Image.open('./Pokedex/pikachu.jpg')
 filtered_img = img.convert('L')
 #filtered_img.show() show image in dailog box
 turned = filtered_img.rotate(45)
-turned.save("grey.png",'png')
+resize = turned.resize((700,700),3)
+resize.save("grey.png",'png')
 
 
 
