e120e1ca9a451e497760c7a3adb3e0885d09d9f8
Avery <AveryGaskins93@gmail.com>
2025-09-09 08:27:31 -0400

    adding more ability to configure image

--- ImageProcessor/images.py
+++ ImageProcessor/images.py
@@ -2,10 +2,12 @@ from PIL import Image, ImageFilter
 
 img = Image.open('./Pokedex/pikachu.jpg')
 filtered_img = img.convert('L')
+region = filtered_img.crop(box=(100,100,400,400))
 #filtered_img.show() show image in dailog box
-turned = filtered_img.rotate(45)
-resize = turned.resize((700,700),3)
-resize.save("grey.png",'png')
+#turned = filtered_img.rotate(45)
+#resize = turned.resize((700,700),3)
+#region = resize.crop((.1,.1,.1,.1))
+region.save("grey.png",'png')
 
 
 
