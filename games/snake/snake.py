c9f7cd60a49cfe584f90b9c60789e5d59948ce41
Avery <AveryGaskins93@gmail.com>
2025-10-10 15:18:31 -0400

    restructuring code to input snake into sprite

---
+++ games/snake/snake.py
@@ -0,0 +1,238 @@
+import random
+import  pygame
+
+
+pygame.init()
+
+WINDOW_WIDTH = 600
+WINDOW_HEIGHT = 600
+display_surface = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
+pygame.display.set_caption("snake XP")
+
+FPS = 20
+clock = pygame.time.Clock()
+
+SNAKE_SIZE = 20
+head_x = WINDOW_WIDTH // 2
+head_y = WINDOW_HEIGHT //2 + 100
+
+snake_dx = 0
+snake_dy = 0
+
+
+
+score = 0
+
+GREEN = (0,255,0)
+DARKGREEN = ( 10, 50, 10)
+RED = ( 255 , 0 , 0)
+DARKRED = ( 150, 0 , 0 )
+WHITE = (255, 255, 255)
+
+font = pygame.font.SysFont('gabriola', 48)
+
+title_text = font.render("Snake", True, GREEN, DARKGREEN)
+title_rect = title_text.get_rect()
+title_rect.center = (WINDOW_WIDTH//2, WINDOW_HEIGHT //2)
+
+score_text = font.render("Score: " + str(score), GREEN, DARKRED)
+score_rect = score_text.get_rect()
+score_rect.topleft = (10,10)
+
+game_over_text = font.render("GAMEOVER", True, RED, DARKGREEN)
+game_over_rect = game_over_text.get_rect()
+game_over_rect.center = (WINDOW_WIDTH//2 , WINDOW_HEIGHT//2)
+
+continue_text = font.render("press any key to play again", True, RED, DARKGREEN)
+continue_rect = continue_text.get_rect()
+continue_rect.center = (WINDOW_WIDTH//2, WINDOW_HEIGHT//2 + 64)
+
+#Sound
+
+#Images
+apple_coord = (500, 500, SNAKE_SIZE, SNAKE_SIZE)
+apple_rect = pygame.draw.rect(display_surface,RED,apple_coord)
+
+head_coord = (head_x, head_y, SNAKE_SIZE , SNAKE_SIZE)
+head_rect = pygame.draw.rect(display_surface,GREEN,head_coord)
+
+body_coords = []
+
+class Snake(pygame.sprite.Sprite):
+    """ Moves a fly trap across the screen, following the moust """
+
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self) # initializing sprite
+        self.snake_dx = 0
+        self.snake_dy = 0
+        self.rect = pygame.draw.rect(display_surface,GREEN,head_coord)
+        self.head_coord = (head_x, head_y, SNAKE_SIZE , SNAKE_SIZE)
+        self.image = pygame.Surface([SNAKE_SIZE,SNAKE_SIZE])
+        self.image.fill(GREEN)
+        self.head_x = WINDOW_WIDTH // 2
+        self.head_y = WINDOW_HEIGHT // 2 + 100
+        self.body_coords = []
+
+    def move_left(self):
+        self.snake_dx = -1 * SNAKE_SIZE
+        self.snake_dy = 0  # move down
+    def move_right(self): # up key
+        self.snake_dx = 1 * SNAKE_SIZE
+        self.snake_dy = 0  # move up
+    def move_up(self):
+        self.snake_dx = 0
+        self.snake_dy = -1 * SNAKE_SIZE  # move right
+    def move_down(self):
+        self.snake_dx = 0
+        self.snake_dy = 1 * SNAKE_SIZE  # move left
+
+    def move_forward(self):
+        self.body_coords.insert(0, self.head_coord)
+        self.body_coords.pop()
+        # update x,y  and make new coordinga
+        self.rect.x += self.snake_dx
+        self.rect.y += self.snake_dy
+        self.head_coord = (self.rect.x, self.rect.y, SNAKE_SIZE, SNAKE_SIZE)
+        # self.body_coords = []
+        # self.size = SNAKE_SIZE
+        # self.head_x = WINDOW_WIDTH // 2
+        # self.head_y = WINDOW_HEIGHT // 2 + 100
+        # self.snake_dx = 0
+        # self.snake_dy = 0
+        # self.grow = False
+    #def update(self):
+        # if self.grow():
+        #     self._grow_body()
+        # else:
+        #     self._move()
+
+    def grow_body(self):
+        self.body_coords.append(self.head_coord)
+
+
+    def draw_body(self):
+        for body in self.body_coords:
+            pygame.draw.rect(display_surface, DARKGREEN, body)
+
+
+
+
+
+
+
+    # def move(self):
+    #     body_coords.insert(0, self.head_coord)
+    #     body_coords.pop()
+    #
+    #     # update x,y  and make new coordinga
+    #     self.head_x += snake_dx
+    #     self.head_y += snake_dy
+    #     self.head_coord = (head_x, head_y, SNAKE_SIZE, SNAKE_SIZE)
+    #
+    def eat(self, target):
+        rect = self.rect
+        return rect.colliderect(target)
+    #
+    # def un_swat(self):
+    #     self.spray = False
+
+snake = Snake()
+snake_sprite = pygame.sprite.GroupSingle(snake)
+
+
+
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running= False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                snake.move_left()
+            if event.key == pygame.K_RIGHT:
+                snake.move_right()
+            if event.key == pygame.K_UP:
+                snake.move_up()
+            if event.key == pygame.K_DOWN:
+                snake.move_down()
+
+    snake.move_forward()
+
+     # add the ehad coordinate to the first index of the body coordinate list
+     # this will move the snakes body by one postion in the list
+    # body_coords.insert(0,head_coord)
+    # body_coords.pop()
+    #
+    # #update x,y  and make new coordinga
+    # head_x += snake_dx
+    # head_y += snake_dy
+    # head_coord = (head_x,head_y,SNAKE_SIZE,SNAKE_SIZE)
+
+    # check for game over
+    if head_rect.left < 0 or head_rect.right > WINDOW_HEIGHT or head_rect.top < 0 or head_rect.bottom > WINDOW_HEIGHT or head_coord in body_coords:
+        display_surface.blit(game_over_text,game_over_rect)
+        display_surface.blit(continue_text,continue_rect)
+        pygame.display.update()
+
+        is_paused = True
+        while is_paused:
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    score = 0
+                    head_x = WINDOW_WIDTH//2
+                    head_y = WINDOW_HEIGHT//2 + 100
+                    head_coord = (head_x, head_y, SNAKE_SIZE, SNAKE_SIZE)
+                    body_coords = []
+
+                    snake_dx = 0
+                    snake_dy = 0
+
+                    is_paused= False
+
+                if event.type == pygame.QUIT:
+                    is_paused = False
+
+
+
+    if snake.eat(apple_rect):
+        score += 1
+        apply_x = random.randint(0 , WINDOW_WIDTH - SNAKE_SIZE)
+        apple_y = random.randint(0, WINDOW_HEIGHT - SNAKE_SIZE)
+        apple_coord= (apply_x,apple_y, SNAKE_SIZE, SNAKE_SIZE)
+
+        snake.grow_body()
+
+
+
+
+    score_text = font.render("Score: "+str(score) , True, GREEN, DARKGREEN)
+
+
+
+
+
+
+
+
+    display_surface.fill(WHITE)
+    #Blit huf
+    display_surface.blit(title_text, title_rect)
+    display_surface.blit(score_text, score_rect)
+
+    #Display Sprites:
+    snake_sprite.update()
+    snake_sprite.draw(display_surface)
+    # mosquito_sprites.update()
+    # mosquito_sprites.draw(surface_display)
+
+    #blit assets
+    snake.draw_body()
+    head_rect = pygame.draw.rect(display_surface,GREEN, head_coord)
+    apple_rect = pygame.draw.rect(display_surface,RED, apple_coord)
+
+    pygame.display.flip()
+    clock.tick(FPS)
+
+
+pygame.quit()
